cd6b000ffd1fc655b9a69f60c925017ece54dd5e
Ming Wong <js923030238@hotmail.com>
2015-10-22 07:14:38 +0800

    認可方案

---
+++ TrendsDataToDB/FormResearch.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TrendsDataToDB
+{
+    partial class FormResearch
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbPath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbPath
+            // 
+            this.txbPath.Location = new System.Drawing.Point(12, 39);
+            this.txbPath.Name = "txbPath";
+            this.txbPath.Size = new System.Drawing.Size(559, 27);
+            this.txbPath.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 6);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(98, 27);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "開啟檔案";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // dlgOpenFile
+            // 
+            this.dlgOpenFile.FileName = ".csv";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(577, 39);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 27);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "匯入";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // FormResearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txbPath);
+            this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormResearch";
+            this.Text = "研究";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbPath;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+        private System.Windows.Forms.Button btnImport;
+    }
+}
+
---
+++ TrendsDataToDB/FormResearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrendsDataToDB
+{
+    public partial class FormResearch : Form
+    {
+        public FormResearch()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dlgOpenFile.ShowDialog();
+            txbPath.Text = dlgOpenFile.FileName;
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(txbPath.Text)) { MessageBox.Show("未選擇檔案路徑！"); return; }
+            String fileText = "";
+            using (StreamReader sr = new StreamReader(txbPath.Text,Encoding.UTF8))
+            {
+                String line;
+                while (sr.Peek() != -1)
+                {
+                    line = sr.ReadLine();
+                    fileText += line;
+                }
+            }
+        }
+    }
+}
